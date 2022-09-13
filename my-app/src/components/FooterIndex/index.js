212bcfecaccc49ee121beae0f9d46c5634fd3a01
Nguyễn Anh Tuấn <anhtuans2783@gmail.com>
2022-09-13 21:35:21 +0700

    git commit -m "FPKNKD-14 Footer

--- my-app/src/components/FooterIndex/index.js
+++ my-app/src/components/FooterIndex/index.js
@@ -1,171 +1,138 @@
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Button } from 'primereact/button'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import './styles.scss'
 
 const FooterIndex = () => {
     return (
         <div>
-            <div className="surface-0  ">
-                <div className="text-900 text font-bold text-6xl mb-4 text-center">Pricing Plans</div>
-                <div className="text-700 text-xl mb-6 text-center line-height-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit numquam eligendi quos.</div>
+            <div className="footer">
+                <div className="grid ">
+                    <div className="col-12 pb-8 lg:col">
+                        <div className=" h-full">
+                            <div className="h-full flex flex-column">
+                                <div className="title">DentiCare</div>
+                                <hr className="hr" />
+                                <div className="text-500 text-xl pt-5">A team of dentists working to ensure you receive the best treatment.
+                                </div>
+                                <ul className="list-none p-0 m-0 flex-grow-1 pt-5">
+                                    <li className="flex align-items-center mb-3">
+                                        <i className="pi pi-phone text-2xl icon"></i>
+                                        <span className='text-2xl'>415-205-5550</span>
+                                    </li>
+                                    <li className="flex align-items-center mb-3">
+                                        <i className="pi pi-envelope text-2xl icon"></i>
+                                        <span className='text-2xl'>emergencies@denticare.com
+                                        </span>
+                                    </li>
+                                    <li className="flex align-items-center mb-3">
+                                        <i className="pi pi-globe text-2xl icon"></i>
+                                        <span className='text-2xl'>denticate.com</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 lg:col">
+                        <div className=" h-full">
+                            <div className="  h-full flex flex-column">
+                                <div className="title">About</div>
+                                <hr className="hr" />
 
-                <div className="grid px-10">
-                    <div className="col-12 lg:col">
-                        <div className="p-3 h-full">
-                            <div className="shadow-2 p-3 h-full flex flex-column" style={{ borderRadius: '6px' }}>
-                                <div className="text-900 font-medium text-xl mb-2">Basic</div>
-                                <div className="text-600">Plan description</div>
-                                <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                                <div className="flex align-items-center">
-                                    <span className="font-bold text-2xl text-900">$9</span>
-                                    <span className="ml-2 font-medium text-600">per month</span>
-                                </div>
-                                <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                                <ul className="list-none p-0 m-0 flex-grow-1">
+                                <ul className="list-none p-0 m-0 flex-grow-1 pt-5">
                                     <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Arcu vitae elementum</span>
+
+                                        <a href='#'><span className='text-2xl'>Our Dental Team</span></a>
                                     </li>
                                     <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Dui faucibus in ornare</span>
+
+                                        <a href='#'><span className='text-2xl'>Pricing & Pricelist</span></a>
                                     </li>
                                     <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Morbi tincidunt augue</span>
+
+                                        <a href='#'><span className='text-2xl'>Solutions</span></a>
+                                    </li>
+                                    <li className="flex align-items-center mb-3">
+
+                                        <a href='#'><span className='text-2xl'>Dental Services</span></a>
                                     </li>
                                 </ul>
-                                <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300 mt-auto" />
-                                <Button label="Buy Now" className="p-3 w-full mt-auto" />
                             </div>
                         </div>
                     </div>
                     <div className="col-12 lg:col">
-                        <div className="p-3 h-full">
-                            <div className="shadow-2 p-3 h-full flex flex-column" style={{ borderRadius: '6px' }}>
-                                <div className="text-900 font-medium text-xl mb-2">Basic</div>
-                                <div className="text-600">Plan description</div>
-                                <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                                <div className="flex align-items-center">
-                                    <span className="font-bold text-2xl text-900">$9</span>
-                                    <span className="ml-2 font-medium text-600">per month</span>
+                        <div className=" h-full">
+                            <div className="  h-full flex flex-column">
+                                <div className="title">Our Awards</div>
+                                <hr className="hr" />
+                                <div className="text-500 text-xl pt-5">DentiCare is a multiple award winner
                                 </div>
-                                <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                                <ul className="list-none p-0 m-0 flex-grow-1">
+                                <ul className="list-none p-0 m-0 flex-grow-1 pt-5">
                                     <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Arcu vitae elementum</span>
+
+                                        <span className='text-2xl'>Best Practice & Team <strong>2016</strong> </span>
                                     </li>
                                     <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Dui faucibus in ornare</span>
+
+                                        <span className='text-2xl'>Best Patient Care <strong>2014</strong>
+                                        </span>
                                     </li>
                                     <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Morbi tincidunt augue</span>
+
+                                        <span className='text-2xl'>Best Patient Care <strong>2017</strong></span>
                                     </li>
                                 </ul>
-                                <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300 mt-auto" />
-                                <Button label="Buy Now" className="p-3 w-full mt-auto" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 lg:col">
+                        <div className=" h-full">
+                            <div className="  h-full flex flex-column">
+                                <div className="title">Social Networks</div>
+                                <hr className="hr" />
+                                <div className="text-500 text-xl pt-5">Visit DentiCare on these social links and connect with us. Make sure to follow our accounts for regular updates.
+                                </div>
+                                <ul className="flex pb-5 pt-7 lg:pt-7">
+                                    <li className="flex align-items-center mb-3">
+                                        <i className="pi pi-facebook text-7xl icon"></i>
+                                    </li>
+                                    <li className="flex align-items-center mb-3">
+                                        <i className="pi pi-youtube text-7xl text-red-500 icon"></i>
+
+                                    </li>
+                                    <li className="flex align-items-center mb-3">
+                                        <i className="pi pi-twitter text-7xl icon"></i>
+
+                                    </li>
+                                    <li className="flex align-items-center mb-3">
+                                        <i className="pi pi-telegram text-7xl icon"></i>
+
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
 
-                    <div className="col-12 lg:col">
-                        <div className="p-3 h-full">
-                            <div className="shadow-2 p-3 h-full flex flex-column" style={{ borderRadius: '6px' }}>
-                                <div className="text-900 font-medium text-xl mb-2">Premium</div>
-                                <div className="text-600">Plan description</div>
-                                <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                                <div className="flex align-items-center">
-                                    <span className="font-bold text-2xl text-900">$29</span>
-                                    <span className="ml-2 font-medium text-600">per month</span>
-                                </div>
-                                <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                                <ul className="list-none p-0 m-0 flex-grow-1">
-                                    <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Arcu vitae elementum</span>
-                                    </li>
-                                    <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Dui faucibus in ornare</span>
-                                    </li>
-                                    <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Morbi tincidunt augue</span>
-                                    </li>
-                                    <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Duis ultricies lacus sed</span>
-                                    </li>
-                                </ul>
-                                <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300" />
-                                <Button label="Buy Now" className="p-3 w-full" />
-                            </div>
-                        </div>
-                    </div>
 
-                    <div className="col-12 lg:col">
-                        <div className="p-3 h-full">
-                            <div className="shadow-2 p-3 flex flex-column" style={{ borderRadius: '6px' }}>
-                                <div className="text-900 font-medium text-xl mb-2">Enterprise</div>
-                                <div className="text-600">Plan description</div>
-                                <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                                <div className="flex align-items-center">
-                                    <span className="font-bold text-2xl text-900">$49</span>
-                                    <span className="ml-2 font-medium text-600">per month</span>
-                                </div>
-                                <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                                <ul className="list-none p-0 m-0 flex-grow-1">
-                                    <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Arcu vitae elementum</span>
-                                    </li>
-                                    <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Dui faucibus in ornare</span>
-                                    </li>
-                                    <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Morbi tincidunt augue</span>
-                                    </li>
-                                    <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Duis ultricies lacus sed</span>
-                                    </li>
-                                    <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Imperdiet proin</span>
-                                    </li>
-                                    <li className="flex align-items-center mb-3">
-                                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                        <span>Nisi scelerisque</span>
-                                    </li>
-                                </ul>
-                                <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300" />
-                                <Button label="Buy Now" className="p-3 w-full p-button-outlined" />
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
-            <div className='flex justify-content-between px-8 bg-blue-500 py-5'>
+            <div className='flex flex-wrap justify-content-between align-items-center subFooter py-5'>
                 <div className='text-white'>Copyright 2020 by <strong>BoldThemes.</strong> All rights reserved.</div>
-                <div className=''>
-                    <div className="">
-                        <div className=" flex gap-5 uppercase text-white ">
-                            <NavLink to="/product" className='text-white' >Sản phẩm</NavLink>
 
-                            <NavLink to="/new" className='text-white' >Tin tức</NavLink>
+                <div className="pt-5 lg:pt-0">
+                    <div className=" flex gap-5 uppercase text-white ">
+                        <NavLink to="/product" className='text-white' >Sản phẩm</NavLink>
 
-                            <NavLink to="#" className='text-white' >Giới Thiệu</NavLink>
-                            <NavLink to="/contact" className='text-white' >Liên hệ</NavLink>
+                        <NavLink to="/new" className='text-white' >Tin tức</NavLink>
 
-                        </div>
+                        <NavLink to="#" className='text-white' >Giới Thiệu</NavLink>
+                        <NavLink to="/contact" className='text-white' >Liên hệ</NavLink>
+
                     </div>
                 </div>
+
 
             </div>
         </div>

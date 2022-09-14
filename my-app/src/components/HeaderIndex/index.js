8be0cee873d0030665bf3af6f8f3a91dcfaf7cef
TruongDucNghia <truongducnghia030802@gmail.com>
2022-09-14 11:46:38 +0700

    FPKNKD-16 <message>

--- my-app/src/components/HeaderIndex/index.js
+++ my-app/src/components/HeaderIndex/index.js
@@ -1,25 +1,162 @@
-import React from 'react'
-import { Button } from 'primereact/button';
-const HaederIndex = () => {
-    return (
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "./styles.module.scss";
+import { Menubar } from "primereact/menubar";
+const cx = classNames.bind(styles);
 
-        <div className="grid grid-nogutter surface-0 text-800">
-            <div className="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
-                <section>
-                    <span className="block text-6xl font-bold mb-1">Create the screens your</span>
-                    <div className="text-6xl text-primary font-bold mb-3">your visitors deserve to see</div>
-                    <p className="mt-0 mb-4 text-700 line-height-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+const items = [
+  {
+    label: "HOME",
+    items: [
+      {
+        label: "New",
+        icon: "pi pi-fw pi-plus",
+        items: [
+          {
+            label: "Bookmark",
+            icon: "pi pi-fw pi-bookmark",
+          },
+          {
+            label: "Video",
+            icon: "pi pi-fw pi-video",
+          },
+        ],
+      },
+      {
+        label: "Delete",
+        icon: "pi pi-fw pi-trash",
+      },
+      {
+        separator: true,
+      },
+      {
+        label: "Export",
+        icon: "pi pi-fw pi-external-link",
+      },
+    ],
+  },
+  {
+    label: "SERVICES",
+    items: [
+      {
+        label: "Left",
+        icon: "pi pi-fw pi-align-left",
+      },
+      {
+        label: "Right",
+        icon: "pi pi-fw pi-align-right",
+      },
+      {
+        label: "Center",
+        icon: "pi pi-fw pi-align-center",
+      },
+      {
+        label: "Justify",
+        icon: "pi pi-fw pi-align-justify",
+      },
+    ],
+  },
+  {
+    label: "CONTACT",
+    items: [
+      {
+        label: "New",
+        icon: "pi pi-fw pi-user-plus",
+      },
+      {
+        label: "Delete",
+        icon: "pi pi-fw pi-user-minus",
+      },
+      {
+        label: "Search",
+        icon: "pi pi-fw pi-users",
+        items: [
+          {
+            label: "Filter",
+            icon: "pi pi-fw pi-filter",
+            items: [
+              {
+                label: "Print",
+                icon: "pi pi-fw pi-print",
+              },
+            ],
+          },
+          {
+            icon: "pi pi-fw pi-bars",
+            label: "List",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: "PROFILE",
+    items: [
+      {
+        label: "Edit",
+        icon: "pi pi-fw pi-pencil",
+        items: [
+          {
+            label: "Save",
+            icon: "pi pi-fw pi-calendar-plus",
+          },
+          {
+            label: "Delete",
+            icon: "pi pi-fw pi-calendar-minus",
+          },
+        ],
+      },
+      {
+        label: "Archieve",
+        icon: "pi pi-fw pi-calendar-times",
+        items: [
+          {
+            label: "Remove",
+            icon: "pi pi-fw pi-calendar-minus",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: "NEWS",
+  },
+];
 
-                    <Button label="Learn More" type="button" className="mr-3 p-button-raised" />
-                    <Button label="Live Demo" type="button" className="p-button-outlined" />
-                </section>
-            </div>
-            <div className="col-12 md:col-6 overflow-hidden">
-                <img src="assets/images/blocks/hero/hero-1.png" alt="hero-1" className="md:ml-auto block md:h-full" style={{ clipPath: 'polygon(8% 0, 100% 0%, 100% 100%, 0 100%)' }} />
-            </div>
+
+const HeaderIndex = () => {
+  return (
+    <div className={cx("wrapper-header")}>
+      <div className={cx("wrapper-header1")}>
+        <div className={cx("wrapper-main-logo")}>
+          <img src="https://res.cloudinary.com/dbpw1enlu/image/upload/v1663124409/denticare-logo_umln3a.png" />
         </div>
+        <div className={cx("wrapper-timeline")}>
+          <div className={cx("wrapper-timer-wroking")}>
+              <div className={cx("timer-img")}>
+                  <img src="https://res.cloudinary.com/dbpw1enlu/image/upload/v1663127402/image_1_tpkkww.png"/>
+              </div>
+              <div className={cx("timer-wrapper-text")}>
+                  <p className={cx("timer-text1")}>415-205-5550</p>
+                  <p className={cx("timer-text2")}>24/7 Emergency Phone</p>
+              </div>
+          </div>
+          <div className={cx("wrapper-timer-wroking")}>
+              <div className={cx("timer-img")}>
+                  <img src="https://res.cloudinary.com/dbpw1enlu/image/upload/v1663127408/image_2_nat93t.png"/>
+              </div>
+              <div className={cx("timer-wrapper-text")}>
+                  <p className={cx("timer-text1")}>Monday - Friday</p>
+                  <p className={cx("timer-text2")}>9:00 AM - 9:00 PM</p>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div className={cx("wrapper-header2")}>
+        <Menubar className={cx("wrapper-menu")} model={items} />
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default HaederIndex
+export default HeaderIndex;
--- my-app/src/layouts/WebsiteLayout/index.js
+++ my-app/src/layouts/WebsiteLayout/index.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import FooterIndex from '../../components/FooterIndex'
-import HaederIndex from '../../components/HeaderIndex'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import FooterIndex from "../../components/FooterIndex";
+import HeaderIndex from "../../components/HeaderIndex";
+import classNames from "classnames/bind";
+import styles from "./styles.module.scss";
 
+const cx = classNames.bind(styles)
 const WebsiteLayout = () => {
-    return (
-        <div>
-            <header>
-                <HaederIndex />
-            </header>
-            <main>
-                <Outlet />
-            </main>
-            <footer>
-                <FooterIndex />
-            </footer>
-        </div>
-    )
-}
+  return (
+    <div className={cx('wrapper-layout')}>
+      <header className={cx('wrapper-main-header')}>
+        <HeaderIndex />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <FooterIndex />
+      </footer>
+    </div>
+  );
+};
 
-export default WebsiteLayout
+export default WebsiteLayout;

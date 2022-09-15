9f6fa96250a3d1a12a59912f0c320a105dc3a6d8
Nguyễn Anh Tuấn <anhtuans2783@gmail.com>
2022-09-15 17:14:53 +0700

    git commit -m "FPKNKD-29 build-page-404

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Test from './components/Test';
 import WebsiteLayout from './layouts/WebsiteLayout';
+import Page404 from './pages/404';
 import HomePage from './pages/HomePage';
 import NewsPage from './pages/News';
 <script src="https://unpkg.com/primereact/primereact.all.min.js"></script>
@@ -13,7 +14,9 @@ function App() {
         <Route path='/' element={<WebsiteLayout />}>
           <Route index element={<HomePage />} />
           <Route path='/news' element={<NewsPage />} />
+          <Route path='*' element={<Page404 />} />
         </Route>
+
       </Routes>
     </div>
   );

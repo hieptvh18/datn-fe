1288b8885c8499e0cb10eb62f48b7f3e49b5bcaa
TruongDucNghia <truongducnghia030802@gmail.com>
2022-09-29 16:33:52 +0700

    FPKNKD-57 done

--- my-app/src/components/HeaderIndex/index.js
+++ my-app/src/components/HeaderIndex/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import classNames from "classnames/bind";
 import styles from "./styles.module.scss";
 import { Menubar } from "primereact/menubar";
@@ -8,132 +8,12 @@ import { Button } from "primereact/button";
 import { useDispatch, useSelector } from "react-redux";
 import { Logout } from "../../feature/AuthSlice";
 import { SplitButton } from 'primereact/splitbutton';
-import { NavLink } from "react-router-dom"
+import { Navigate, NavLink, useNavigate } from "react-router-dom"
 const cx = classNames.bind(styles);
 
 
-
-const items = [
-  {
-    label: "HOME",
-    items: [
-      {
-        label: "New",
-        icon: "pi pi-fw pi-plus",
-        items: [
-          {
-            label: "Bookmark",
-            icon: "pi pi-fw pi-bookmark",
-          },
-          {
-            label: "Video",
-            icon: "pi pi-fw pi-video",
-          },
-        ],
-      },
-      {
-        label: "Delete",
-        icon: "pi pi-fw pi-trash",
-      },
-      {
-        separator: true,
-      },
-      {
-        label: "Export",
-        icon: "pi pi-fw pi-external-link",
-      },
-    ],
-  },
-  {
-    label: "SERVICES",
-    items: [
-      {
-        label: "Left",
-        icon: "pi pi-fw pi-align-left",
-      },
-      {
-        label: "Right",
-        icon: "pi pi-fw pi-align-right",
-      },
-      {
-        label: "Center",
-        icon: "pi pi-fw pi-align-center",
-      },
-      {
-        label: "Justify",
-        icon: "pi pi-fw pi-align-justify",
-      },
-    ],
-  },
-  {
-    label: "CONTACT",
-    items: [
-      {
-        label: "New",
-        icon: "pi pi-fw pi-user-plus",
-      },
-      {
-        label: "Delete",
-        icon: "pi pi-fw pi-user-minus",
-      },
-      {
-        label: "Search",
-        icon: "pi pi-fw pi-users",
-        items: [
-          {
-            label: "Filter",
-            icon: "pi pi-fw pi-filter",
-            items: [
-              {
-                label: "Print",
-                icon: "pi pi-fw pi-print",
-              },
-            ],
-          },
-          {
-            icon: "pi pi-fw pi-bars",
-            label: "List",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: "PROFILE",
-    items: [
-      {
-        label: "Edit",
-        icon: "pi pi-fw pi-pencil",
-        items: [
-          {
-            label: "Save",
-            icon: "pi pi-fw pi-calendar-plus",
-          },
-          {
-            label: "Delete",
-            icon: "pi pi-fw pi-calendar-minus",
-          },
-        ],
-      },
-      {
-        label: "Archieve",
-        icon: "pi pi-fw pi-calendar-times",
-        items: [
-          {
-            label: "Remove",
-            icon: "pi pi-fw pi-calendar-minus",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: "NEWS",
-  },
-];
-
-
 const HeaderIndex = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const isUser = useSelector(data => data.user.value.user)
   const infoAccount = [
@@ -146,6 +26,33 @@ const HeaderIndex = () => {
       url: '/account/info'
     }
   ]
+  const items = [
+    {
+      label: 'Trang chủ',
+      command: (event) => {
+        navigate('/')
+      }
+    },
+    {
+      label: "Về chúng tôi",
+      command: (event) => {
+        navigate('/about')
+      }
+    },
+    {
+      label: "Bài viết",
+      command: (event) => {
+        navigate('/news')
+      }
+    },
+    {
+      label: "Bệnh án",
+      command: (event) => {
+        navigate('/infoaccount')
+      }
+    }
+  ];
+  
   return (
     <div className={cx("wrapper-header")}>
       <div className={cx("wrapper-header1")}>

635fdc5679549089dfbf2ceca6769ceb3c7280fd
TruongDucNghia <truongducnghia030802@gmail.com>
2022-10-05 10:49:25 +0700

    FPKNKD-66 done responsive profile

--- my-app/src/components/SpecialiezdTeam/index.js
+++ my-app/src/components/SpecialiezdTeam/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { listTeam } from '../../feature/TeamSlice';
-
+import {NavLink} from 'react-router-dom'
+import './styles.scss'
 const SpecialiezdTeam = () => {
     const team = useSelector(team => team.team.value);
     const dispatch = useDispatch()
@@ -10,16 +11,22 @@ const SpecialiezdTeam = () => {
     }, []);
     return (
         <div className="mx-auto w-default px-5">
-                <div className="flex column-gap-5 -mt-8">
+                <div className="flex flex-column md:flex-row row-gap-6 md:column-gap-5 -mt-8 px-7 md:px-0">
                     {team?.map((item) => {
-                        return <div className="surface-200 w-full">
+                        return <div className="surface-200 w-full list-4top-item">
                             <div className="w-full">
                                 <div className="w-full flex flex-column">
-                                    <div className="w-full">
+                                    <div className="w-full relative box-img">
                                         <img
                                             className='flex w-full'
                                             src={item.image}
                                         />
+                                        <NavLink to={''}>
+                                            <button className='bg-primary1 text-white font-normal px-4 py-3 text-2xl border-round-lg pointer absolute btn-read-more'>
+                                                Read More 
+                                                <i className='pi pi-angle-right text-2xl ml-2'></i>
+                                            </button>
+                                        </NavLink>
                                     </div>
                                     <div className="px-6">
                                         <div className="title pt-5 font-medium">{item.name}</div>

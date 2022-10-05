9add50aca3f9c11bfed0b94c9bc1ffb9cff73b23
Nguyễn Anh Tuấn <anhtuans2783@gmail.com>
2022-10-05 20:57:45 +0700

    FPKNKD-72 api list top4 service

--- my-app/src/api/service.js
+++ my-app/src/api/service.js
@@ -9,3 +9,8 @@ export const getServiceById = (id) => {
     const url = `services/${id}`
     return instance.get(url)
 }
+
+export const getListTop4 = () => {
+    const url = `services?_sort=createdAt&_order=asc&_limit=4`
+    return instance.get(url)
+}

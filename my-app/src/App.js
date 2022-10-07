719015fc99435b3d2acc9c95b57b8368ba0d60f8
Nguyễn Anh Tuấn <anhtuans2783@gmail.com>
2022-10-07 16:55:48 +0700

    git commit -m "FPKNKD-81 xay dung trang huong dan khach hang

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ import WebsiteLayout from './layouts/WebsiteLayout';
 import Page404 from './pages/404';
 import AboutPage from './pages/About';
 import AccountInfo from './pages/AccountInfo';
+import CustomerGuide from './pages/CustomerGuide';
 import HomePage from './pages/HomePage';
 import NewsPage from './pages/News';
 import NewsDetail from './pages/NewsDetail';
@@ -21,7 +22,8 @@ function App() {
           <Route path='news/detail' element={<NewsDetail />} />
           <Route path='about' element={<AboutPage />} />
           <Route path='infoAccount' element={<AccountInfo />} />
-          <Route path='teams' element={<Team />} />
+          <Route path='doi-ngu-nhan-vien' element={<Team />} />
+          <Route path='huong-dan-khac-hang' element={<CustomerGuide />} />
           <Route path='*' element={<Page404 />} />
         </Route>
 

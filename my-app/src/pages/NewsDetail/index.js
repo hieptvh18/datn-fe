a95747c66fc250423c307acade998ed97547b270
TruongDucNghia <truongducnghia030802@gmail.com>
2022-12-14 22:44:24 +0700

    FPKNKD-143 done

--- my-app/src/pages/NewsDetail/index.js
+++ my-app/src/pages/NewsDetail/index.js
@@ -18,35 +18,35 @@ const NewsDetail = () => {
     return (
         <>
             {/* Banner */}
-            <div className='w-full'>
+            <div className='w-full cs-img-newss'>
                 <div className='imageBanner'>
                     <div className='cell'>
-                        <div className='cellNews'>
-                            <div className='flex align-items-center gap-4 '>
+                        <div className='cellNews w-default mx-auto'>
+                            <div className='flex align-items-center gap-4 px-5 md:px-7'>
                                 <div className='my-2'>
                                     <i className="pi pi-tags text-4xl tags-icon"></i>
-                                    <span className='text-1xl white pr-3'>News</span> |
+                                    <span className='text-1xl white pr-3'>Tin Tức</span> |
                                 </div>
                                 <div className='my-2'>
-                                    <span className='text-1xl white pr-3'>Prevention</span> |
+                                    <span className='text-1xl white pr-3'>Tiện lợi</span> |
                                 </div>
                                 <div className='my-2'>
-                                    <span className='text-1xl white'>Text</span>
+                                    <span className='text-1xl white'>Phòng Ngừa</span>
                                 </div>
                                 <div className='my-2'>
                                     <i className="pi pi-comment text-4xl tags-icon"></i>
                                 </div>
                             </div>
-                            <div className='pb-3 text-8xl'>{newsDetail.data?.title}</div>
-                            <hr className='hr py-1 w-2 mb-7' />
-                            <div className='flex align-items-center gap-6 '>
+                            <div className='pb-3 text-6xl md:text-8xl px-7'>{newsDetail?.data?.title}</div>
+                            <hr className='hr py-1 w-2 mb-7 mx-7' />
+                            <div className='flex align-items-center gap-6 px-7'>
                                 <div className='my-2 align-items'>
                                     <i className="pi pi-clock text-3xl tags-icon"></i>
-                                    <span className='text-1xl white'>{newsDetail.data?.created_at}</span>
+                                    <span className='text-1xl white'>{newsDetail?.data?.created_at}</span>
                                 </div>
                                 <div className='my-2 align-items'>
                                     <i className="pi pi-user text-3xl tags-icon"></i>
-                                    <span className='text-1xl white'>by {newsDetail.data?.news_author.fullname}
+                                    <span className='text-1xl white'>by {newsDetail?.data?.news_author?.fullname}
                                     </span>
                                 </div>
                             </div>
@@ -55,10 +55,10 @@ const NewsDetail = () => {
                 </div>
                 <div className='content paddingBottom'>
                     <div className='flex flex-wrap gap-8'>
-                        <div className='col-8'>
-                            <div dangerouslySetInnerHTML={{ __html: newsDetail.data?.content }} />
+                        <div className='col-12 w-default mx-auto px-8'>
+                            <div dangerouslySetInnerHTML={{ __html: newsDetail?.data?.content }} />
                         </div>
-                        <WidgetNew />
+                        {/* <WidgetNew /> */}
                     </div>
 
                 </div>

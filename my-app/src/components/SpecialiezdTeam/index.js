de261453af97280f24ab1574200e9ed5058d10ae
Nguyễn Anh Tuấn <anhtuans2783@gmail.com>
2022-12-25 19:57:48 +0700

    fix

--- my-app/src/components/SpecialiezdTeam/index.js
+++ my-app/src/components/SpecialiezdTeam/index.js
@@ -13,56 +13,54 @@ const SpecialiezdTeam = () => {
         <div className="mx-auto w-default px-5">
             <div className="flex flex-column md:flex-row row-gap-6 md:column-gap-5 -mt-8 px-7 md:px-0">
                 {team.data?.map((item, index) => {
-                    if (index < 4) {
+                    if (index > 4) {
                         return <div className="surface-200 w-full list-4top-item" key={index}>
-                            <div className="w-full">
-                                <div className="w-full flex flex-column">
-                                    <div className="w-full relative box-img">
-                                        {item?.avatar ?
-                                            <img
-                                                className='flex w-full'
-                                                src={'http://nhakhoaducnghia.xyz/' + item?.avatar}
-                                            />
-                                            : <img
-                                                className='flex w-full'
-                                                src='http://denticare.bold-themes.com/allen/wp-content/uploads/sites/16/2020/01/people-01.jpg'
-                                            />
-                                        }
-                                        <NavLink to={'/doi-ngu-nhan-vien'}>
-                                            <button className='bg-primary1 text-white font-normal px-4 py-3 text-2xl border-round-lg pointer absolute btn-read-more'>
-                                                Read More
-                                                <i className='pi pi-angle-right text-2xl ml-2'></i>
-                                            </button>
-                                        </NavLink>
+                            <div className="">
+                                <div className="box-img">
+                                    {item?.avatar ?
+                                        <img
+                                            height="300px"
+                                            className='flex w-full'
+                                            src={'http://nhakhoaducnghia.xyz/' + item?.avatar}
+                                        />
+                                        : <img
+                                            height="300px"
+                                            className='flex w-full'
+                                            src='http://denticare.bold-themes.com/allen/wp-content/uploads/sites/16/2020/01/people-01.jpg'
+                                        />
+                                    }
+                                    <NavLink to={'/doi-ngu-nhan-vien'}>
+                                        <button className='bg-primary1 text-white font-normal px-4 py-3 text-2xl border-round-lg pointer absolute btn-read-more'>
+                                            Read More
+                                            <i className='pi pi-angle-right text-2xl ml-2'></i>
+                                        </button>
+                                    </NavLink>
+                                </div>
+                                <div className="px-5">
+                                    <div style={{ fontFamily: 'var(--fontRoboto)' }} className="title pt-5 font-medium" >{item.fullname}</div>
+                                    <hr className="hr" />
+                                    <div className="text-600 text-2xl pt-5">
+                                        {item.specialist}
                                     </div>
-                                    <div className="px-6">
-                                        <div style={{ fontFamily: 'var(--fontRoboto)' }} className="title pt-5 font-medium" >{item.fullname}</div>
-                                        <hr className="hr" />
-                                        <div className="text-600 text-2xl pt-5">
-                                            {item.specialist}
-                                        </div>
-                                        <div className="text-600 text-2xl pt-5">
-                                            Giám đốc bệnh viện Thẩm mỹ Hàn Quốc 2016- 2018 chuyên sâu thẩm mỹ toàn diện cơ thể ngực, bụng, khuôn mặt.,
-                                            Tốt nghiệp khoa chỉnh hình tại Úc năm 2004.,
-                                            Tốt nghiệp Chuyên khoa Phẫu thuật hàm mặt 1999-2010 tại Viện Răng Hàm Mặt Trung Ương TP.HCM.,
-                                            Tốt nghiệp Thạc sĩ năm 2007.,
-                                            Học Tiến sĩ năm 2007-2010.
-                                        </div>
-                                        <ul className="flex pb-5 pt-7 lg:pt-7">
-                                            <li className="flex align-items-center mb-3">
+                                    <div className="text-600 text-2xl pt-5" dangerouslySetInnerHTML={{ __html: item?.description?.slice(0, 400) }}>
+                                    </div>
+                                    <div className=''>
+                                        <ul style={{ listStyle: "none" }} className="flex pb-5 pt-7 lg:pt-7">
+                                            <li className="">
                                                 <i className="pi pi-facebook text-5xl icon"></i>
                                             </li>
-                                            <li className="flex align-items-center mb-3">
+                                            <li className="">
                                                 <i className="pi pi-youtube text-5xl text-red-500 icon"></i>
                                             </li>
-                                            <li className="flex align-items-center mb-3">
+                                            <li className="">
                                                 <i className="pi pi-twitter text-5xl icon"></i>
                                             </li>
-                                            <li className="flex align-items-center mb-3">
+                                            <li className="">
                                                 <i className="pi pi-telegram text-5xl icon"></i>
                                             </li>
                                         </ul>
                                     </div>
+
                                 </div>
                             </div>
                         </div>
